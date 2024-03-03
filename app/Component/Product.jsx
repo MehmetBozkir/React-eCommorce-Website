9e387d580e24afa8b7047bf99c79e025905bc488
Mehmet Salih Bozkır <150898451+MehmetBozkir@users.noreply.github.com>
2024-03-04 00:11:28 +0300

    Update Product.jsx

--- app/Component/Product.jsx
+++ app/Component/Product.jsx
@@ -15,6 +15,19 @@ function Product() {
   const [shopdata, setShopData] = useState([]);
   const [searchh, setSearchh] = useState("");
   const [loading, setLoading] = useState(true);
+  const [productCounts, setProductCounts] = useState({});
+
+  const totalProductsCount = Object.values(productCounts).reduce(
+    (acc, count) => acc + count,
+    0
+  );
+  const totalPrice = Object.values(productCounts).reduce(
+    (acc, count, index) => {
+      const productPrice = shopdata[index].price;
+      return acc + count * productPrice;
+    },
+    0
+  );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -47,8 +60,58 @@ function Product() {
           className="input w-80 mt-5 bg-gray-100"
           onChange={(e) => setSearchh(e.target.value)}
         />
+
         <div className="text-2xl mt-7 ml-2 ">
           <IoSearch />
+        </div>
+        <div className="flex-none mt-5 bg-white border-4 rounded-full ml-5">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle"
+            >
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                  />
+                </svg>
+                <span className="badge badge-sm indicator-item text-white bg-red-600">
+                  {" "}
+                  {totalProductsCount}{" "}
+                </span>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+            >
+              <div className="card-body">
+                <span className="font-bold text-lg ">
+                  {" "}
+                  {totalProductsCount} Items
+                </span>
+                <span className="text-info">
+                  Subtotal: $ {totalPrice.toFixed(2)}
+                </span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    Pay Screen
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="grid grid-cols-4 gap-5 ml-48 mr-20 ">
@@ -63,6 +126,11 @@ function Product() {
               class="card w-full bg-base-100 shadow-xl mt-10 border-8"
               key={uuidv4()}
             >
+              {productCounts[Product.id] && (
+                <span className="ml-auto badge-sm text-white rounded-bl-lg bg-black">
+                  {productCounts[Product.id]}{" "}
+                </span>
+              )}
               <Link href={`/product/${Product.id}`}>
                 <img
                   className="mx-auto transition duration-300 ease-in-out hover:scale-125 mt-10"
@@ -73,7 +141,7 @@ function Product() {
                 />
               </Link>
               <div class="card-body">
-                <h1 class="card-title ">{Product.title}</h1>
+                <h1 class="card-title ">{Product.title} </h1>
                 <p className="text-sm">{Product.description}</p>
                 <div class="card-actions justify-end">
                   <div class="flex text-left mt-5">
@@ -81,7 +149,15 @@ function Product() {
                     <p class="ml-2">{Product.price} $</p>
                   </div>
 
-                  <button onClick={notify} class="btn btn-primary">
+                  <button
+                    onClick={() => {
+                      const updatedCounts = { ...productCounts };
+                      updatedCounts[Product.id] =
+                        (updatedCounts[Product.id] || 0) + 1;
+                      setProductCounts(updatedCounts);
+                    }}
+                    class="btn btn-primary"
+                  >
                     Buy Now
                   </button>
                   <Toaster position="bottom-left" reverseOrder={false} />
